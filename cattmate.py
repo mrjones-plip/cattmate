344b4719c298c19c3d1d98ab7ce51f87fa7125e0
Ths2-9Y-LqJt6 <Ths2-9Y-LqJt6@plip.com>
2019-12-07 22:59:20 -0800

    refactor __main__, add comments

--- cattmate.py
+++ cattmate.py
@@ -1,5 +1,12 @@
-import time, json, os.path, config
-from filelock import Timeout, FileLock
+import time, config
+from filelock import FileLock
+
+
+def get_volume_from_file():
+    lock = FileLock(config.volumefile_lock, timeout=1)
+    with lock:
+        volume = open(config.volumefile, "r").read()
+    return volume
 
 
 def create_volume_file():
@@ -9,32 +16,27 @@ def create_volume_file():
 
 
 def main():
-    if not os.path.isfile(config.volumefile):
-        create_volume_file()
 
+    # make sure we have a good file and volume
     try:
-        lock = FileLock(config.volumefile_lock, timeout=1)
-        with lock:
-            volume = open(config.volumefile, "r").read()
+        volume = get_volume_from_file()
     except TypeError:
         create_volume_file()
         volume = '10'
 
+    # remember current volume and let use know we're starting
     current_volume = volume
-
     print('start: ' + current_volume)
 
+    # enter endless loop to check file for volume updates
     while True:
-        lock = FileLock(config.volumefile_lock, timeout=1)
-        with lock:
-            volume = open(config.volumefile, "r").read()
-
+        volume = get_volume_from_file()
         if volume != current_volume:
             current_volume = volume
             print('update: ' + volume)
 
+        # wait a certain amount of time so we don't over load the system with file reads
         time.sleep(config.refresh_wait)
-
 
 if __name__ == "__main__":
     main()
