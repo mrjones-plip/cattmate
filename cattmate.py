aafb3d4490fc23eee772cb40c58bb155ac8f9d9f
Ths2-9Y-LqJt6 <Ths2-9Y-LqJt6@plip.com>
2019-12-09 13:25:57 -0800

    try and get name, v4 or v6 handle for chromecast, pretty print volume

--- cattmate.py
+++ cattmate.py
@@ -1,4 +1,5 @@
-import time, cattmate_config, config
+import time, cattmate_config, config, socket, sys
+from os import system
 import catt.api as cat_api
 from filelock import FileLock
 
@@ -21,6 +22,19 @@ def milli_time():
     return int(round(time.time() * 1000))
 
 
+def get_cast_handle(name_or_ip):
+    try:
+        socket.inet_pton(socket.AF_INET6, config.chromecasts[0])
+        cast = cat_api.CattDevice(ip_addr=config.chromecasts[0])
+    except socket.error:
+        try:
+            socket.inet_aton(config.chromecasts[0])
+            cast = cat_api.CattDevice(ip_addr=config.chromecasts[0])
+        except socket.error:
+            cast = cat_api.CattDevice(name=config.chromecasts[0])
+    return cast
+
+
 def main():
     # make sure we have a good file and volume
     try:
@@ -34,9 +48,15 @@ def main():
     last_volume_update = milli_time()
     need_update = False
 
+    # todo - check for IPv6 too socket.inet_pton(socket.AF_INET6, some_string)
     print('Trying to start with: ' + str(config.chromecasts[0]))
-    cast = cat_api.CattDevice(name=config.chromecasts[0])
-    print('start: ' + current_volume)
+    try:
+        cast = get_cast_handle(config.chromecasts[0])
+    except cat_api.CastError:
+        sys.exit("ERROR: Could not connect to '" + config.chromecasts[0] + "'. Check config.py and name/IP and try again.")
+
+    _ = system('clear')
+    print(current_volume)
 
     # enter endless loop to check file for volume updates
     while True:
@@ -45,14 +65,14 @@ def main():
             current_volume = volume
             last_volume_update = milli_time()
             need_update = True
-            print('update: ' + volume)
+            _ = system('clear')
+            print(volume)
 
         # wait 400ms since last local volume change before sending update to chromecast
         if need_update & (milli_time() - last_volume_update > 400):
             need_update = False
             cast_volume = int(volume)/100;
             cast.volume(cast_volume)
-
             print('send vol update: ' + str(cast_volume))
 
         # wait a certain amount of time so we don't over load the system with file reads
